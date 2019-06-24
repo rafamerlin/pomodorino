bb43fac4fd73c50d986257c26140ff6e39ce6094
Merurino <rafamerlin@gmail.com>
2019-06-24 23:02:31 +1000

    Add Cancel button and fix notifications

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ let alerts: Alerts = null;
 let pomo: PomoEngine = null;
 
 function appReady() {
+  //We need to add this for windows to behave properly: https://github.com/electron/electron/issues/10864
+  //This fixes the notifications for example.
+  app.setAppUserModelId("Pomodorino"); //Must be the same as build.appId in package.json
+  
   let invisibleRenderer = new BrowserWindow(
     {
       width: 100,
@@ -20,7 +24,7 @@ function appReady() {
     })
   invisibleRenderer.loadURL(`file://${dir}/src/renderer.html`);
   alerts = new Alerts(invisibleRenderer, `${dir}`);
-  pomo = new PomoEngine(alerts, GenerateContextMenu(), dir);
+  pomo = new PomoEngine(alerts, generateContextMenu(), dir);
 }
 
 // This method will be called when Electron has finished
@@ -45,7 +49,13 @@ app.on('activate', () => {
   // }
 })
 
-function GenerateContextMenu(): Menu {
+function cleanUpAndQuit(){
+  alerts = null;
+  pomo = null;
+  app.quit();
+}
+
+function generateContextMenu(): Menu {
   return Menu.buildFromTemplate([
     {
       id: '900', label: "Config", type: "submenu", submenu: [
@@ -58,6 +68,7 @@ function GenerateContextMenu(): Menu {
     { id: '15', label: '15', type: 'normal', click: _ => pomo.startPomodoro(15) },
     { id: '5', label: '5', type: 'normal', click: _ => pomo.startPomodoro(5) },
     { type: 'separator' },
-    { id: '-1', label: 'Quit', type: 'normal', click: _ => app.quit() },
+    { id: '-2', label: 'Cancel', type: 'normal', click: _ => pomo.reset() },
+    { id: '-1', label: 'Quit', type: 'normal', click: _ => cleanUpAndQuit() },
   ]);
 }
